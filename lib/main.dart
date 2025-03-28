e07cf8ef50fbcdbf539c88a468e7639c858b1fbb
Ankit <sam.megh0305@gmail.com>
2025-03-28 21:41:16 +0530

    firbase is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:mychat/core/utils/app_theam_data.dart';
 import 'package:mychat/presentation/screens/auth/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
