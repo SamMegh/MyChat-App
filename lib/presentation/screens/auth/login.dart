c4189fdd10b4a52ce52ee83c9153025c4cabc2ed
Ankit <sam.megh0305@gmail.com>
2025-03-28 12:27:36 +0530

    sign up page is ready

--- lib/presentation/screens/auth/login.dart
+++ lib/presentation/screens/auth/login.dart
@@ -11,88 +11,136 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreen extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController emailcontroller = TextEditingController();
   final TextEditingController passwordcontroller = TextEditingController();
+bool hidePassword = true;
+  
+  final _emailFocus = FocusNode();
+  final _passwordFocus = FocusNode();
 
   @override
   void dispose() {
     emailcontroller.dispose();
     passwordcontroller.dispose();
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
     super.dispose();
+  }
+
+  String? _emailValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a Email';
+    }
+    if (RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+        ).hasMatch(value) ==
+        false) {
+      return 'Enter a valid email';
+    }
+
+    return null;
+  }
+
+  String? _passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a Password';
+    }
+    if (value.length < 6) {
+      return 'Password should be atleast of 6 characters';
+    }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 50),
-              Text(
-                "Welcome Back!",
-                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "login to continue",
-                style: Theme.of(
-                  context,
-                ).textTheme.bodyMedium?.copyWith(color: Colors.grey),
-              ),
-              SizedBox(height: 30),
-              InputBox(
-                controller: emailcontroller,
-                hintText: "Email",
-                prefixIcon: Icon(Icons.email_outlined),
-              ),
-              SizedBox(height: 10),
-              InputBox(
-                controller: passwordcontroller,
-                hintText: "Password",
-                prefixIcon: Icon(Icons.lock_outline_rounded),
-                suffixIcon: Icon(Icons.remove_red_eye_outlined),
-                obscureText: true,
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Forget Password",
-                style: TextStyle(color: Theme.of(context).primaryColor),
-              ),
-              Center(child: CoustomButton(text: "Login", onPressed: () {})),
-              SizedBox(height: 20),
-              Center(
-                child: RichText(
-                  text: TextSpan(
-                    text: "Need an account! ",
-                    style: TextStyle(color: Colors.grey),
-                    children: [
-                      TextSpan(
-                        text: "Signup",
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        recognizer:
-                            TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const SignupScreen(),
-                                  ),
-                                );
-                              },
-                      ),
-                    ],
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  "Welcome Back!",
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Text(
+                  "login to continue",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodyMedium?.copyWith(color: Colors.grey),
+                ),
+                SizedBox(height: 30),
+                InputBox(
+                  controller: emailcontroller,
+                  hintText: "Email",
+                  focusNode: _emailFocus,
+                  validator: _emailValidator,
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                SizedBox(height: 10),
+                InputBox(
+                  controller: passwordcontroller,
+                  hintText: "Password",
+                  focusNode: _passwordFocus,
+                  validator: _passwordValidator,
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                   suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          hidePassword = !hidePassword;
+                        });
+                      },
+                      icon: Icon(hidePassword?Icons.visibility_off_outlined:Icons.visibility_outlined),
+                    ),
+                    obscureText: hidePassword,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Forget Password",
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+                Center(child: CoustomButton(text: "Login", onPressed: () {
+                  FocusScope.of(context).unfocus();
+                    if (_formKey.currentState?.validate() ?? false) {}
+                })),
+                SizedBox(height: 20),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Need an account! ",
+                      style: TextStyle(color: Colors.grey),
+                      children: [
+                        TextSpan(
+                          text: "Signup",
+                          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const SignupScreen(),
+                                    ),
+                                  );
+                                },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/presentation/screens/auth/signup.dart
+++ lib/presentation/screens/auth/signup.dart
@@ -11,18 +11,87 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreen extends State<SignupScreen> {
-  final TextEditingController FullNameController = TextEditingController();
-  final TextEditingController UserNameController = TextEditingController();
-  final TextEditingController EmailController = TextEditingController();
-  final TextEditingController PhoneController = TextEditingController();
-  final TextEditingController PasswordContoller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  bool hidePassword = true;
+
+  // all Controller
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordContoller = TextEditingController();
+  // all focus nodes
+  final _fullNameFocus = FocusNode();
+  final _userNameFocus = FocusNode();
+  final _emailFocus = FocusNode();
+  final _phoneNumberFocus = FocusNode();
+  final _passwordFocus = FocusNode();
+
+  // all validators
+  String? _fullNameValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a Full Name';
+    }
+    return null;
+  }
+
+  String? _userNameValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a User Name';
+    }
+    if (value.length < 3) {
+      return 'User Name must be of minimum 3 characters';
+    }
+    return null;
+  }
+
+  String? _emailValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a Email';
+    }
+    if (RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+        ).hasMatch(value) ==
+        false) {
+      return 'Enter a valid email';
+    }
+
+    return null;
+  }
+
+  String? _phoneNumberValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a Phone Number';
+    }
+    if (RegExp(r'[0-9]{10}$').hasMatch(value) == false) {
+      return 'Invalid Phone Number';
+    }
+    return null;
+  }
+
+  String? _passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a Password';
+    }
+    if (value.length < 6) {
+      return 'Password should be atleast of 6 characters';
+    }
+    return null;
+  }
+
   @override
   void dispose() {
-    FullNameController.dispose();
-    UserNameController.dispose();
-    EmailController.dispose();
-    PhoneController.dispose();
-    PasswordContoller.dispose();
+    fullNameController.dispose();
+    userNameController.dispose();
+    emailController.dispose();
+    phoneController.dispose();
+    passwordContoller.dispose();
+    _fullNameFocus.dispose();
+    _userNameFocus.dispose();
+    _emailFocus.dispose();
+    _phoneNumberFocus.dispose();
+    _passwordFocus.dispose();
     super.dispose();
   }
 
@@ -30,84 +99,112 @@ class _SignupScreen extends State<SignupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Create an Account",
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              SizedBox(height: 5),
-              Text(
-                "Please fill the details to continue",
-                style: Theme.of(
-                  context,
-                ).textTheme.bodySmall?.copyWith(color: Colors.grey),
-              ),
-              SizedBox(height: 30),
-              InputBox(
-                controller: FullNameController,
-                hintText: "Full Name",
-                prefixIcon: Icon(Icons.person_outline),
-              ),
-              SizedBox(height: 10),
-              InputBox(
-                controller: UserNameController,
-                hintText: "Username",
-                prefixIcon: Icon(Icons.alternate_email),
-              ),
-              SizedBox(height: 10),
-              InputBox(
-                controller: EmailController,
-                hintText: "Email",
-                prefixIcon: Icon(Icons.email_outlined),
-              ),
-              SizedBox(height: 10),
-              InputBox(
-                controller: PhoneController,
-                hintText: "Phone Number",
-                prefixIcon: Icon(Icons.phone_outlined),
-              ),
-              SizedBox(height: 10),
-              InputBox(
-                controller: PasswordContoller,
-                hintText: "Password",
-                prefixIcon: Icon(Icons.lock_outline),
-                suffixIcon: Icon(Icons.remove_red_eye_outlined),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              CoustomButton(onPressed: () {}, text: "Signup"),
-              SizedBox(height: 10),
-              Center(
-                child: RichText(
-                  text: TextSpan(
-                    text: "Already have an account? ",
-                    children: [
-                      TextSpan(
-                        text: "Login",
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                          color: Theme.of(context).primaryColor,
-                          fontWeight: FontWeight.bold
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Create an Account",
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                SizedBox(height: 5),
+                Text(
+                  "Please fill the details to continue",
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodySmall?.copyWith(color: Colors.grey),
+                ),
+                SizedBox(height: 30),
+                InputBox(
+                  controller: fullNameController,
+                  hintText: "Full Name",
+                  focusNode: _fullNameFocus,
+                  validator: _fullNameValidator,
+                  prefixIcon: Icon(Icons.person_outline),
+                ),
+                SizedBox(height: 10),
+                InputBox(
+                  controller: userNameController,
+                  hintText: "Username",
+                  focusNode: _userNameFocus,
+                  validator: _userNameValidator,
+                  prefixIcon: Icon(Icons.alternate_email),
+                ),
+                SizedBox(height: 10),
+                InputBox(
+                  controller: emailController,
+                  hintText: "Email",
+                  focusNode: _emailFocus,
+                  validator: _emailValidator,
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+                SizedBox(height: 10),
+                InputBox(
+                  controller: phoneController,
+                  hintText: "Phone Number",
+                  focusNode: _phoneNumberFocus,
+                  validator: _phoneNumberValidator,
+                  prefixIcon: Icon(Icons.phone_outlined),
+                ),
+                SizedBox(height: 10),
+                InputBox(
+                  controller: passwordContoller,
+                  hintText: "Password",
+                  focusNode: _passwordFocus,
+                  validator: _passwordValidator,
+                  prefixIcon: Icon(Icons.lock_outline),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        hidePassword = !hidePassword;
+                      });
+                    },
+                    icon: Icon(hidePassword?Icons.visibility_off_outlined:Icons.visibility_outlined),
+                  ),
+                  obscureText: hidePassword,
+                ),
+                SizedBox(height: 20),
+                CoustomButton(
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    if (_formKey.currentState?.validate() ?? false) {}
+                  },
+                  text: "Create Account",
+                ),
+                SizedBox(height: 10),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Already have an account? ",
+                      children: [
+                        TextSpan(
+                          text: "Login",
+                          style: Theme.of(
+                            context,
+                          ).textTheme.bodyLarge?.copyWith(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const LoginScreen(),
+                                    ),
+                                  );
+                                },
                         ),
-                        recognizer:
-                           TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const LoginScreen(),
-                                  ),
-                                );
-                              },
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
