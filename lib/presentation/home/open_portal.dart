e4f18f529373a2867b98d23f8b7e8e30aadf9338
Ankit <sam.megh0305@gmail.com>
2025-04-17 17:08:06 +0530

    bottom navbar is added

--- lib/presentation/home/open_portal.dart
+++ lib/presentation/home/open_portal.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:mychat/presentation/chat/profile_screen.dart';
+import 'package:mychat/presentation/home/home.dart';
 
 class OpenPortal extends StatefulWidget {
   const OpenPortal({super.key});
+  static final List<Widget> _screen = [
+    Home(),
+    ProfileScreen()
+  ];
 
   @override
   State<OpenPortal> createState() => _OpenPortalState();
 }
 
 class _OpenPortalState extends State<OpenPortal> {
+  int _currentIndex = 0;
+  void onTap(int i) {
+    _currentIndex = i;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("openPortal")));
+    return Scaffold(
+      body: OpenPortal._screen[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTap,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.manage_accounts_outlined),
+          ),
+        ],
+      ),
+    );
   }
 }
